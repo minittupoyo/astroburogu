94f3082b9ee2a63614d28e05d70c219fe6574540
minittupoyo <minittu.4989@gmail.com>
2026-04-25 05:51:08 +0900

    feat: enhance blog layout and functionality
    - Updated blog post component for improved readability and structure.
    - Added new dependencies for font and icon support in package.json.
    - Refactored header component to include navigation links.
    - Removed unused Welcome component to streamline the project.
    - Improved blog index page to display all posts with a count.
    - Enhanced global styles with new font settings.
    - Configured Prettier for consistent code formatting.
    - Added VSCode settings for better development experience.
    - Updated TypeScript configuration for stricter checks.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import remarkBreaks from 'remark-breaks';
+import remarkBreaks from "remark-breaks";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
+
   markdown: {
-    remarkPlugins: [remarkBreaks]
-  }
+    remarkPlugins: [remarkBreaks],
+  },
+
+  integrations: [icon()],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,17 @@
 import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
-import { z } from "zod"
+import { z } from "zod";
 
 const blog = defineCollection({
-    loader: glob({ base: "./content/blog", pattern: "**/*.md" }),
-    schema:({ image }) => z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.coerce.date(),
-        cover: image().optional(),
-        draft: z.boolean().default(false)
-    })
-})
+  loader: glob({ base: "./content/blog", pattern: "**/*.md" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.coerce.date(),
+      cover: image().optional(),
+      draft: z.boolean().default(false),
+    }),
+});
 
 export const collections = { blog };
